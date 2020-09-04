d67af4c168e0ed8398218339a92439571c8fe234
David <friday10292001@yahoo.com.tw>
2020-09-04 21:10:25 +0800

    product list

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,9 +1,54 @@
 class Admin::ProductsController < Admin::BaseController
+  before_action :find_product, only: [:edit, :update, :destroy]
   def index
+    @products = Product.all
   end
 
   def new
     @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_products_path, notice: '商品新增成功!'
+    else
+      render :new
+    end
+  end
+  
+  def edit
+  end
+
+  def update
+    @product.update(product_params)
+    if @product.save
+      redirect_to edit_admin_product_path(@product), notice: '商品更新成功!'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to admin_products_path, notice: '商品已刪除'
+  end
+
+
+  
+
+
+  private
+  def product_params
+    params.require(:product).permit(:name,
+                                   :vendor_id,
+                                   :list_price,
+                                   :sell_price,
+                                   :on_sell,
+                                   :description)
+  end
+  def find_product
+    @product = Product.friendly.find(params[:id])
   end
 
 end
