d8ca9bcb2cead16d9b57d165dee2768b4e2085a8
David <friday10292001@yahoo.com.tw>
2020-09-04 15:43:07 +0800

    vendor part 1

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root 'products#index'
     resources :products 
+    resources :vendors, except: [:show]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
