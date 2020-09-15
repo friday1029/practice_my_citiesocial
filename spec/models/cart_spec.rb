dbb3e89932acbd6692a82a599c0eb843e1484067
David <friday10292001@yahoo.com.tw>
2020-09-15 15:31:31 +0800

    cart part 4 product is Product 2.43

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -9,10 +9,17 @@ RSpec.describe Cart, type: :model do
     end
     it "加了相同種類的商品到購物車裡，購買項目（CartItem）並不會增加，但商品的數量會改變" do
       cart = Cart.new
-      3.times{cart.add_item(1)}
-      2.times{cart.add_item(2)}
+      3.times { cart.add_item(1) }
+      2.times { cart.add_item(2) }
       expect(cart.items.first.quantity).to be 3
       expect(cart.items.last.quantity).to be 2
+    end
+    it "商品可以放到購物車裡，也可以再拿出來" do
+      cart = Cart.new
+      v1 = Vendor.create(title: 'ptt')
+      p1 = Product.create(name: "kk", list_price: '20', sell_price: '2', vendor: v1)
+      cart.add_item(p1.id)
+      expect(cart.items.first.product).to be_a Product
     end
   end
 end
