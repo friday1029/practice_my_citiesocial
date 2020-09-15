ae5741a250fb242d94b73c1dbc44384abddb562c
David <friday10292001@yahoo.com.tw>
2020-09-15 22:15:25 +0800

    cart part10 code refactoring 2.49

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,21 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
+  let(:cart){ Cart.new}
   describe "基本功能" do
     it "可以把商品丟到到購物車裡，然後購物車裡就有東西了。" do
-      cart = Cart.new
       cart.add_item(2)
-      expect(cart.empty?).to be false
+      expect(cart).not_to be_empty
     end
     it "加了相同種類的商品到購物車裡，購買項目（CartItem）並不會增加，但商品的數量會改變" do
-      cart = Cart.new
       3.times { cart.add_item(1) }
       2.times { cart.add_item(2) }
       expect(cart.items.first.quantity).to be 3
       expect(cart.items.last.quantity).to be 2
     end
     it "商品可以放到購物車裡，也可以再拿出來" do
-      cart = Cart.new
       # v1 = Vendor.create(title: 'ptt')
       # p1 = Product.create(name: "kk", list_price: '20', sell_price: '2', vendor: v1)
       p1 = FactoryBot.create(:product)
@@ -23,7 +21,6 @@ RSpec.describe Cart, type: :model do
       expect(cart.items.first.product).to be_a Product
     end
     it "可以計算整台購物車的總消費金額" do
-      cart = Cart.new
       p1 = create(:product, sell_price:5)
       p2 = create(:product, sell_price:10)
       3.times { cart.add_item(p1.id)}
@@ -34,7 +31,6 @@ RSpec.describe Cart, type: :model do
   end
   describe "進階功能" do
     it "可以將購物車內容轉換成 Hash 並存到 Session 裡" do
-      cart = Cart.new
       p1 = create(:product, sell_price:5)
       p2 = create(:product, sell_price:10)
       3.times { cart.add_item(p1.id)}
